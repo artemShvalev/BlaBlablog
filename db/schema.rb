12067ca9fe023b674e3993e4c857a35887b561f0
Vasilisa Tyulberova <vasiliqa13@gmail.com>
2020-04-18 19:15:55 +0700

    Fix devise settings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200408121936) do
+ActiveRecord::Schema.define(version: 20200407100204) do
 
   create_table "comments", force: :cascade do |t|
     t.string "username"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20200408121936) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
